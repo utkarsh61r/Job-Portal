501b9c035845d2253218841973c7ece1c4463155
Utkarsh Upadhyay <upadhyayutkarsh1614@gmail.com>
2025-12-24 16:29:00 +0530

    logo changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,30 +4,45 @@ import { useClerk,UserButton,useUser } from '@clerk/clerk-react'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
-   const { openSignIn } = useClerk()
-   const { user } = useUser()
-
-
-
+  const { openSignIn } = useClerk()
+  const { user } = useUser()
 
   return (
-    <div className="shadow py-4">
-      <div className='container px-4 2xl:px-20 mx-auto flex justify-between'>
-      <img  src={assets.logo} alt="logo" />
-      { user 
-      ? <div>
-        <Link to={'/application'}>Applied Jobs</Link>
-        <p>Hii {user.firstName+" "+user.lastName}</p>
-        <UserButton />
-      </div> 
-      :<div className='flex gap-4 max-sm:text-xs'>
-      <button className='text-gray-600'>Recuiter Login</button>
-      <button onClick={ e=> openSignIn()} className='bg-blue-600 text-white px-6 sm:px-9 rounded-full'>Login</button>
-    </div>
-    }
-    </div>
+    <div className="shadow h-15 flex items-center">
+      <div className="container px-6 mx-auto flex justify-between items-center">
+
+       
+        <img
+          src={assets.logo}
+          alt="Job Dekho"
+          className="h-14 max-w-[200px] object-contain"
+        />
+
+       
+        {user ? (
+          <div className="flex items-center gap-3 text-sm">
+            <Link to="/application">Applied Jobs</Link>
+            <span>|</span>
+            <span>Hi, {user.firstName} {user.lastName}</span>
+            <UserButton />
+          </div>
+        ) : (
+          <div className="flex gap-4 text-sm">
+            <button className="text-gray-600">Recruiter Login</button>
+            <button
+              onClick={openSignIn}
+              className="bg-blue-600 text-white px-6 rounded-full"
+            >
+              Login
+            </button>
+          </div>
+        )}
+
+      </div>
     </div>
   )
 }
+
+
 
 export default Navbar
